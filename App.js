7c38ee514b7e41f16c9bde7d30e9667553e686ad
SandraIpiales <112151095+SandraIpiales@users.noreply.github.com>
2024-01-24 11:15:22 -0500

    cambio Contacts Estudiante 1

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name='WellcomeSandraI' component={Home} />
-        <Stack.Screen name='ContactsNav' component={Contacts} />
+        <Stack.Screen name='CallNumber' component={Contacts} />
         <Stack.Screen name='ProductNavigation' component={Product}/>
       </Stack.Navigator>
     </NavigationContainer>
