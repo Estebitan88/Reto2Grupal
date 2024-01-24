ff15969be700073104001e21a02132cfe680223c
SandraIpiales <112151095+SandraIpiales@users.noreply.github.com>
2024-01-24 10:49:02 -0500

    cambio de nombres 2 Estudiante 1

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
     <NavigationContainer>
 
       <Stack.Navigator>
-        <Stack.Screen name='WellcomeSandra' component={Home} />
+        <Stack.Screen name='WellcomeSandraI' component={Home} />
         <Stack.Screen name='ContactsNav' component={Contacts} />
         <Stack.Screen name='ProductNavigation' component={Product}/>
 
--- app/screens/ContactsScreen.js
+++ app/screens/ContactsScreen.js
@@ -6,7 +6,7 @@ export const Contacts = ({navigation}) => {
         <Button
             title='HOME'
             onPress={()=>{
-                navigation.navigate('WellcomeSandra');
+                navigation.navigate('WellcomeSandraI');
             }}
         />
     </View>
