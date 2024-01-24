6eccacc8ae79c732b773652396d6370dbac620b5
SandraIpiales <112151095+SandraIpiales@users.noreply.github.com>
2024-01-24 09:41:45 -0500

    Cambios en Producto.js Estudiante 1

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -2,7 +2,11 @@ import {View, Text, StyleSheet, Button} from 'react-native'
 
 export const Product = ({navigation}) =>{
     return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
+        <Text>Bienvenidos a mi tienda, soy Sandra Ipiales</Text>
+        <Text style={{ color: 'brown' , backgroundColor:'white', fontSize:20 }}>Chocolate</Text>
+        <Text style={{ color: 'purple', backgroundColor:'white', fontSize:20 }}>Gaseosa</Text>
+        <Text style={{ color: 'green',backgroundColor:'white' , fontSize:20 }}>Helado</Text>
+   
         <Button
             title='HOME'
             onPress={()=>{
@@ -16,7 +20,7 @@ export const Product = ({navigation}) =>{
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#9BE4E9',
       alignItems: 'center',
       justifyContent: 'center',
     },
